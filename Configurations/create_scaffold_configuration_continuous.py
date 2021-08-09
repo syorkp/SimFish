8c54074a1d83b974889dd4568f63f2c078cb87e5
Sam Pink <sampinky1999@gmail.com>
2021-08-09 20:48:38 +0100

    Ready to test 2

--- Configurations/create_scaffold_configuration_continuous.py
+++ Configurations/create_scaffold_configuration_continuous.py
@@ -31,7 +31,7 @@ env = {'width': 750,  # arena size
        'prey_mass': 1.,
        'prey_inertia': 40.,
        'prey_size': 4.,
-       'prey_num': 30,
+       'prey_num': 45,
        'prey_impulse': 0.0,  # impulse each prey receives per step
        'prey_impulse_rate': 0.25,  # fraction of prey receiving impulse per step
        'prey_escape_impulse': 2,
