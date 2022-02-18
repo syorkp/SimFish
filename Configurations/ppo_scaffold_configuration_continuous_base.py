053ddd2f2fb3abaca8cee7abac31799ace3e1cb6
syorkp <sampinky1999@gmail.com>
2022-02-18 08:23:41 +0000

    Second attempt at long term tests - observation back to normal scaling 2

--- Configurations/ppo_scaffold_configuration_continuous_base.py
+++ Configurations/ppo_scaffold_configuration_continuous_base.py
@@ -228,7 +228,7 @@ env = {'width': 1500,  # arena size
        }
 
 
-from Networks.modular_network_2 import connectivity, reflected, base_network_layers, modular_network_layers, ops
+from Networks.original_network import connectivity, reflected, base_network_layers, modular_network_layers, ops
 
 params = {'num_actions': 10,  # size of action space
           'batch_size': 1,  # How many experience traces to use for each training step.
