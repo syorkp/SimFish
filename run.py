c7fa9de9898eee22677c62e151959b862ccf076b
syorkp <sampinky1999@gmail.com>
2022-03-20 23:06:45 +0000

    DQN scaffold 3 trial

--- run.py
+++ run.py
@@ -2092,5 +2092,5 @@ dqn_scaffold_training_config_3b = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_scaffold_training_config_3b, parallel_jobs=4)
+manager = TrialManager(dqn_scaffold_training_config_3a, parallel_jobs=4)
 manager.run_priority_loop()
