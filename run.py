2d1822abec7e60787a18402b586df57fd0412520
syorkp <sampinky1999@gmail.com>
2022-04-05 15:23:00 +0100

    Ready for PPO8aON

--- run.py
+++ run.py
@@ -2931,5 +2931,5 @@ ppo_scaffold_training_config_8b_on = [
 ]
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_scaffold_training_config_8b_on, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_training_config_8a_on, parallel_jobs=4)
 manager.run_priority_loop()
