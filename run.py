9daba67b5006bc704b9f638aa7319aac71968465
syorkp <sampinky1999@gmail.com>
2022-04-07 14:02:58 +0100

    Ready for dqn 9b

--- run.py
+++ run.py
@@ -3239,5 +3239,5 @@ dqn_scaffold_training_config_9b = [
 ]
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_scaffold_training_config_9a, parallel_jobs=4)
+manager = TrialManager(dqn_scaffold_training_config_9b, parallel_jobs=4)
 manager.run_priority_loop()
