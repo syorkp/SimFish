16a34ca374841aa16402f8e6fde88b77fcecccf2
syorkp <sampinky1999@gmail.com>
2022-04-20 12:52:17 +0100

    Ready for ppo_egf_10b

--- run.py
+++ run.py
@@ -2286,5 +2286,5 @@ ppo_testing = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_scaffold_training_config_egf_10a, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_training_config_egf_10b, parallel_jobs=4)
 manager.run_priority_loop()
