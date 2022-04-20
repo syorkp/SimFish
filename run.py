06a068f1fcd52bcc5838825c101e2c3ec5b90241
syorkp <sampinky1999@gmail.com>
2022-04-20 10:27:59 +0100

    Ready for ppo_hs_10b

--- run.py
+++ run.py
@@ -2211,5 +2211,5 @@ ppo_testing = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_scaffold_training_config_high_sigma_10a, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_training_config_high_sigma_10b, parallel_jobs=4)
 manager.run_priority_loop()
