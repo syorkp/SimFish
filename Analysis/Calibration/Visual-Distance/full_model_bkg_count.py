ef198952aa77b763356ff021653b594ade6cc4e7
syorkp <sampinky1999@gmail.com>
2022-04-21 14:22:35 +0100

    DQN_dn_10b

---
+++ Analysis/Calibration/Visual-Distance/full_model_bkg_count.py
@@ -0,0 +1,15 @@
+"""
+Script to directly compute photons from bkg_scatter given all bkg_scatter, decay, arena dims, luminance.
+"""
+
+import numpy as np
+from Tools.drawing_board_new import NewDrawingBoard
+from Environment.Fish.eye import Eye
+
+
+def get_max_bkg_scatter(bkg_scatter, width, height):
+    # Set fish position in corner.
+    fish_position = np.array([1, 1])
+    fish_orientation =
+
+    ...
