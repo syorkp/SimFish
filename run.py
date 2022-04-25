576a6146f2deaa532a5becff785f1e86047737dc
syorkp <sampinky1999@gmail.com>
2022-04-25 14:36:24 +0100

    ppo_max_actual_10a

--- run.py
+++ run.py
@@ -2703,5 +2703,5 @@ ppo_testing_normal_sigma = [
 ]
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_scaffold_training_config_11b, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_training_config_egf_max_actual_10a, parallel_jobs=4)
 manager.run_priority_loop()
