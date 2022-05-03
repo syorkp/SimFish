33a07751e252f94ae59c85933f7931fc65d4d64f
syorkp <sampinky1999@gmail.com>
2022-05-03 20:13:42 +0100

    DQN_12b

--- run.py
+++ run.py
@@ -3260,5 +3260,5 @@ ppo_scaffold_egf_bn_min_ms_wr_10a = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_scaffold_egf_bn_min_ms_wr_10a, parallel_jobs=4)
+manager = TrialManager(dqn_scaffold_training_config_12b, parallel_jobs=4)
 manager.run_priority_loop()
