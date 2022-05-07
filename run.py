01996069cae787c3518f0fadb6cafa2e6c68d80d
syorkp <sampinky1999@gmail.com>
2022-05-07 12:32:22 +0100

    PPO_im_10

--- run.py
+++ run.py
@@ -3446,5 +3446,5 @@ dqn_scaffold_13b = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_scaffold_13b, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_training_config_egf_min_ms_wf_im_10a, parallel_jobs=4)
 manager.run_priority_loop()
