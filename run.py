cec99eb086b01d657be9a10155ee685adb1fb988
syorkp <sampinky1999@gmail.com>
2022-05-18 20:23:52 +0100

    DQN_14-1 Data gathering

--- run.py
+++ run.py
@@ -4111,5 +4111,5 @@ ppo_scaffold_17a = [
 ]
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_assay_test, parallel_jobs=4)
+manager = TrialManager(ppo_scaffold_17a, parallel_jobs=4)
 manager.run_priority_loop()
