8583f4758020f3217e79e8f216c310d844cfb9a4
syorkp <sampinky1999@gmail.com>
2022-06-08 09:18:39 +0100

    DQN assays

--- Services/DQN/dqn_assay_service.py
+++ Services/DQN/dqn_assay_service.py
@@ -109,8 +109,8 @@ class DQNAssayService(AssayService, BaseDQN):
             o, a, r, internal_state, o1, d, rnn_state = self.step_loop(o=o, internal_state=internal_state,
                                                                        a=action_reafference, rnn_state=rnn_state)
             o = o1
-
-            print(self.step_number)
+            if self.step_number % 100 == 0:
+                print(self.step_number)
 
             if d:
                 break
