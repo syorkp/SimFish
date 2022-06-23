6bdb0fe998fdc53bb3d635127b2d5dc6a81e0cb3
syorkp <sampinky1999@gmail.com>
2022-06-23 15:08:54 +0100

    PPO_20

--- run.py
+++ run.py
@@ -12538,5 +12538,5 @@ ppo_scaffold_20_bs = [
 ]
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_scaffold_19, parallel_jobs=3)
+manager = TrialManager(ppo_scaffold_20, parallel_jobs=3)
 manager.run_priority_loop()
