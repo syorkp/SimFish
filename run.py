8df8bdd8ce3ec33c89cefc07b30a6f56016a3dd6
syorkp <sampinky1999@gmail.com>
2022-06-24 14:08:16 +0100

    DQN_23b

--- run.py
+++ run.py
@@ -12613,5 +12613,5 @@ dqn_scaffold_23b = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(dqn_scaffold_23a, parallel_jobs=3)
+manager = TrialManager(dqn_scaffold_23b, parallel_jobs=3)
 manager.run_priority_loop()
