f7dd5d0713ba1e4f1d9f551a721b49d25e5320ea
syorkp <sampinky1999@gmail.com>
2022-07-21 11:47:50 +0100

    PPO_18 data gathering

--- Buffers/PPO/ppo_buffer_continuous_multivariate.py
+++ Buffers/PPO/ppo_buffer_continuous_multivariate.py
@@ -147,7 +147,9 @@ class PPOBufferContinuousMultivariate(BasePPOBuffer):
 
     def save_assay_data(self, assay_id, data_save_location, assay_configuration_id, internal_state_order=None,
                         salt_location=None):
-        hdf5_file, assay_group = BasePPOBuffer.save_assay_data(self, assay_id, data_save_location, assay_configuration_id)
+        hdf5_file, assay_group = BasePPOBuffer.save_assay_data(self, assay_id, data_save_location, assay_configuration_id,
+                                                               internal_state_order=internal_state_order,
+                                                               salt_location=salt_location)
 
         if "environmental positions" in self.recordings:
             self.create_data_group("mu_impulse", np.array(self.mu_i_buffer)[:, 0], assay_group)
