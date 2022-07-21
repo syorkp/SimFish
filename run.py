5323a9cf96c8fc5088f823079e076a39b9b04096
syorkp <sampinky1999@gmail.com>
2022-07-21 11:48:40 +0100

    DQN_28

--- run.py
+++ run.py
@@ -9611,5 +9611,5 @@ ppo_assay_18 = [
 
 
 print(f"Start time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
-manager = TrialManager(ppo_assay_18, parallel_jobs=3)
+manager = TrialManager(dqn_scaffold_28a, parallel_jobs=3)
 manager.run_priority_loop()
