92d9f3ae3d888661bf87f6ce5c95359d276f3028
syorkp <sampinky1999@gmail.com>
2022-08-07 18:44:19 +0100

    DQN_28

--- run.py
+++ run.py
@@ -10121,6 +10121,7 @@ dqn_scaffold_27_1 = [
         "Profile Speed": False,
     },
 ]
+
 dqn_scaffold_27_2 = [
     {
         "Model Name": "dqn_scaffold_27",
@@ -10141,10 +10142,48 @@ dqn_scaffold_27_2 = [
     },
 ]
 
+dqn_scaffold_28_1 = [
+    {
+        "Model Name": "dqn_scaffold_28",
+        "Environment Name": "dqn_scaffold_28",
+        "Trial Number": 1,
+        "Run Mode": "Training",
+        "Tethered": False,
+        "Realistic Bouts": True,
+        "Continuous Actions": False,
+        "Learning Algorithm": "DQN",
+        "Priority": 1,
+        "Using GPU": True,
+        "monitor gpu": False,
+        "Full Logs": True,
+        "SB Emulator": False,
+        "New Simulation": True,
+        "Profile Speed": False,
+    },
+]
+dqn_scaffold_28_2 = [
+    {
+        "Model Name": "dqn_scaffold_28",
+        "Environment Name": "dqn_scaffold_28",
+        "Trial Number": 2,
+        "Run Mode": "Training",
+        "Tethered": False,
+        "Realistic Bouts": True,
+        "Continuous Actions": False,
+        "Learning Algorithm": "DQN",
+        "Priority": 1,
+        "Using GPU": True,
+        "monitor gpu": False,
+        "Full Logs": True,
+        "SB Emulator": False,
+        "New Simulation": True,
+        "Profile Speed": False,
+    },
+]
 
 
 if run_config is None:
-    run_config = dqn_scaffold_27_2
+    run_config = dqn_scaffold_28_1
 else:
     print(f"{run_config} entered.")
     run_config = globals()[run_config]
