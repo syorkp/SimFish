cbf01f47ec31a40f2b70214aa982106f0056a909
syorkp <sampinky1999@gmail.com>
2022-08-26 13:36:45 +0100

    DQN conv data gathering 2

--- Analysis/Neural/CNN/estimate_mei.py
+++ Analysis/Neural/CNN/estimate_mei.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow.compat.v1 as tf
 
 from Analysis.load_data import load_data
+from Analysis.Neural.CNN.graphs_for_mei import MEICore, MEIReadout, Trainer
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 
@@ -19,6 +20,7 @@ def get_all_observations(model_name, assay_config, assay_id, n):
 
     return np.array(observations)
 
+
 def get_all_cnn_activity(model_name, assay_config, assay_id, n):
     keys_to_collect = ["conv1l", "conv2l", "conv3l", "conv4l", "conv1r", "conv2r", "conv3r", "conv4r"]
     collected_data = {}
@@ -32,8 +34,46 @@ def get_all_cnn_activity(model_name, assay_config, assay_id, n):
     return collected_data
 
 
+def normalise_cnn_data(cnn_data):
+    for layer in cnn_data.keys():
+        max_activity = np.max(cnn_data[layer], axis=(0, 1))
+        cnn_data[layer] /= max_activity
+    return cnn_data
+
+
+def build_model(observation_data, activity_data):
+    with tf.Session() as sess:
+        # Creating graph
+        core = MEICore()
+        readout = MEIReadout(core.output)
+        trainer = Trainer(readout.predicted_neural_activity)
+
+        # Init variables
+        init = tf.global_variables_initializer()
+        trainables = tf.trainable_variables()
+
+        sess.run(init)
+
+        compiled_loss = []
+
+        for step in range(observation_data.shape[0]):
+            _, loss = sess.run([trainer.train, trainer.total_loss],
+                            feed_dict={
+                                core.observation: observation_data[step:step+1],
+                                trainer.actual_responses: activity_data[step:step+1]
+                            })
+            compiled_loss.append(loss)
+    return compiled_loss
+
+
 if __name__ == "__main__":
     cnn_activity = get_all_cnn_activity("dqn_scaffold_18-1", "Behavioural-Data-CNN", "Naturalistic", 1)
     observations = get_all_observations("dqn_scaffold_18-1", "Behavioural-Data-CNN", "Naturalistic", 1)
+    cnn_activity = normalise_cnn_data(cnn_activity)
+    observations = observations.astype(float) / 255
 
+    relevant_observations = observations[:, :, :, 0]
+    selected_activity_data = cnn_activity["conv1l"][:, 0, 0]
+    # TODO: should actually find way of flattening across the data dimension (22), while repeating the relevant obsevations
+    loss_data = build_model(relevant_observations, selected_activity_data)
 
