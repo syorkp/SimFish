79ba1795c0f7924111c5abe40ba296910b4bc8b7
syorkp <sampinky1999@gmail.com>
2022-09-05 09:50:02 +0100

    Improved phototaxis index

--- Analysis/Neural/Gradients/get_average_inputs.py
+++ Analysis/Neural/Gradients/get_average_inputs.py
@@ -54,8 +54,34 @@ def get_average_input_during_context(data, labels):
     return mean_observation, mean_rnn_state, mean_energy_state, mean_salt_input, inputted_action, inputted_in_light
 
 
+
+def get_average_input_during_context_multiple_trials(datas, labels):
+    """Takes one hot encoded event and returns the average input during this period."""
+    labels = np.array(labels)
+
+    observations
+    rnn_state
+    salt
+    energy_state
+    actions
+    in_light
+
+    for data, label in zip(datas, labels):
+        observations = data["observation"][labels == 1]
+        rnn_state = data["rnn_state_actor"][labels == 1]
+        salt = data["salt"][labels == 1]
+        energy_state = data["energy_state"][labels == 1]
+        actions = data["action"][labels == 1]
+        in_light = data["in_light"][labels == 1]
+
+    mean_observation, mean_rnn_state, mean_energy_state, mean_salt_input, inputted_action, inputted_in_light = \
+        get_most_common_network_inputs(observations, rnn_state, energy_state, salt, actions, in_light)
+
+    return mean_observation, mean_rnn_state, mean_energy_state, mean_salt_input, inputted_action, inputted_in_light
+
+
 if __name__ == "__main__":
     data = load_data("dqn_scaffold_14-1", "Behavioural-Data-Free", "Naturalistic-1")
-    x = get_average_input_during_context(data, [1 for i in range(len(data["action"]))])
+    x = get_average_input_during_context(data, [1 for i in range(1000)] + [0 for i in range(1000)])
 
 
