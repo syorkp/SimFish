25f7aa8d9014d029e7b95f39b8650d150b048d89
syorkp <sampinky1999@gmail.com>
2022-09-20 21:28:51 +0100

    Built interventions for PPO, and ready to gather interruptions for all models

--- Analysis/Behavioural/Interventions/assess_full_interruptions.py
+++ Analysis/Behavioural/Interventions/assess_full_interruptions.py
@@ -1,7 +1,28 @@
+import numpy as np
 
+from Analysis.load_data import load_data
+
+
+def compute_action_heterogeneity_continuous(actions):
+    # Bin actions
+    # Call method below.
+    ...
+
+
+def compute_action_heterogeneity_discrete(actions):
+    """Returns score from 0 onwards. As heterogeneity increases, so does the score."""
+    action_counts = np.bincount(actions)
+    differences = [np.max(np.delete(action_counts, i) - action_counts[i]) for i in range(action_counts.shape[0])]
+    difference_sum = np.sum(np.absolute(differences))
+
+    heterogeneity = actions.shape[0]/difference_sum
+    heterogeneity -= 0.142857142857142836
+    return heterogeneity
 
 
 if __name__ == "__main__":
-    ...
+    d = load_data("dqn_scaffold_18-1", "Interruptions-A", "Naturalistic-1")
+    h1 = compute_action_heterogeneity_discrete(d["action"][:20])
+    h2 = compute_action_heterogeneity_discrete(d["action"][400:])
 
 
