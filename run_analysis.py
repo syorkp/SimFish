1cd9d0d20f5174459526fd23a92fa919cc6096f8
syorkp <sampinky1999@gmail.com>
2022-10-09 21:02:39 +0100

    MEI Run test

--- run_analysis.py
+++ run_analysis.py
@@ -7,8 +7,8 @@ try:
 except IndexError:
     run_config = None
 
-produce_meis("dqn_scaffold_26-2", "rnn_in", True, 1000, conv=False)
-# produce_meis_extended("dqn_scaffold_26-2", "conv1l", True, 1000)
+# produce_meis("dqn_scaffold_26-2", "rnn_in", True, 1000, conv=False)
+produce_meis_extended("dqn_scaffold_26-2", "conv1l", True, 1000)
 # produce_meis_extended("dqn_scaffold_26-2", "conv2l", True, 1000)
 # produce_meis_extended("dqn_scaffold_26-2", "conv3l", True, 1000)
 # produce_meis_extended("dqn_scaffold_26-2", "conv4l", True, 1000)
