745702e505f31891dbcdbdf2ea1163cf5ae2f299
syorkp <sampinky1999@gmail.com>
2022-10-09 22:38:40 +0100

    PPO Video data

--- run.py
+++ run.py
@@ -21590,7 +21590,7 @@ ppo_video_data = [
         "Continuous Actions": True,
         "Learning Algorithm": "PPO",
         "Priority": 2,
-        "Using GPU": False,
+        "Using GPU": True,
         "monitor gpu": False,
         "Full Logs": True,
         "SB Emulator": True,
