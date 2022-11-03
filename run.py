fd6855a1de866e52967d207448aa811c7dccf8c2
syorkp <sampinky1999@gmail.com>
2022-11-03 17:20:57 +0000

    Ready for testing PPO.

--- run.py
+++ run.py
@@ -28045,7 +28045,7 @@ ppo_new_scaffold_test = [
         "Continuous Actions": True,
         "Learning Algorithm": "PPO",
         "Priority": 1,
-        "Using GPU": False,
+        "Using GPU": True,
         "monitor gpu": False,
         "Full Logs": True,
         "SB Emulator": True,
