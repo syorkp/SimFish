7c6e5762685d178583dff1f6db6ea846e0b5ab66
SamPinky <sampinky1999@gmail.com>
2022-11-07 16:30:05 +0000

    Check assay state loading

--- Buffers/PPO/base_ppo_buffer.py
+++ Buffers/PPO/base_ppo_buffer.py
@@ -279,6 +279,7 @@ class BasePPOBuffer:
         if self.use_dynamic_network:
             for layer in self.unit_recordings.keys():
                 self.create_data_group(layer, np.array(self.unit_recordings[layer]), assay_group)
+            self.create_data_group("rnn_state_actor", np.array(self.actor_rnn_state_buffer), assay_group)
 
         if "environmental positions" in self.recordings:
             self.create_data_group("impulse", np.array(self.action_buffer)[:, 0], assay_group)
