79095d8d22d3594a37aee7bee731e9a666bba2f5
sam <sampinky1999@gmail.com>
2023-01-16 14:21:54 +0000

    fixes

--- Environment/naturalistic_environment.py
+++ Environment/naturalistic_environment.py
@@ -246,7 +246,6 @@ class NaturalisticEnvironment(BaseEnvironment):
                 self.survived_attack = False
 
 
-
         # Relocate fish (Assay mode only)
         if self.relocate_fish is not None:
             if self.relocate_fish[self.num_steps]:
