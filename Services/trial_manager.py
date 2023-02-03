faed901e1562031ca6f5ed7410f915db4220a1ab
syorkp <sampinky1999@gmail.com>
2023-02-03 16:26:35 +0000

    Attempted hybrid mode fix

--- Services/trial_manager.py
+++ Services/trial_manager.py
@@ -533,8 +533,8 @@ class TrialManager:
                     else:
                         print("New job failed")
 
-                    print(running_jobs.keys())
-                    while len(running_jobs.keys()) > self.parallel_jobs - 1 and to_delete is None:
+                    print(f"JOBS: {running_jobs.keys()} - {self.parallel_jobs})
+                    while len(running_jobs.keys()) > self.parallel_jobs - 1:# and to_delete is None:
                         for process in running_jobs.keys():
                             if running_jobs[process].is_alive():
                                 pass
