fixed sand grain uv vs red location, reduced scaling factors

Environment/Fish/eye.py
@@ -229,33 +229,31 @@ class Eye:
                                                        channel_angles_surrounding=channel_angles_surrounding,
                                                        n_channels_uv=self.uv_photoreceptor_num,
                                                        n_channels_red=self.red_photoreceptor_num)
+        
+        uv_items = np.concatenate((prey_positions, sand_grain_positions), axis=0)
 
-        if proj and (len(prey_positions) + len(sand_grain_positions) > 0):
+        if proj and (len(uv_items)) > 0:
             proj_uv_readings = self._read_prey_proj_parallel(eye_x=eye_x,
                                                              eye_y=eye_y,
                                                              uv_pr_angles=self.uv_photoreceptor_angles,
                                                              fish_angle=fish_angle,
                                                              rf_size=self.uv_photoreceptor_rf_size,
                                                              lum_mask=lum_mask,
-                                                             prey_pos=self.chosen_math_library.array(prey_positions))
+                                                             prey_pos=self.chosen_math_library.array(uv_items))
 
             uv_readings += proj_uv_readings
 
             if len(sand_grain_positions) > 0:
-                proj_readings_sand_grains = self._read_prey_proj_parallel(eye_x=eye_x,
+                red_readings_sand_grains = self._read_prey_proj_parallel(eye_x=eye_x,
                                                                           eye_y=eye_y,
-                                                                          uv_pr_angles=self.uv_photoreceptor_angles,
+                                                                          uv_pr_angles=self.red_photoreceptor_angles,
                                                                           fish_angle=fish_angle,
-                                                                          rf_size=self.uv_photoreceptor_rf_size,
+                                                                          rf_size=self.red_photoreceptor_rf_size,
                                                                           lum_mask=lum_mask,
                                                                           prey_pos=self.chosen_math_library.array(sand_grain_positions)
                                                                           )
 
-                proj_uv_readings_sand_grains = self.env_variables["sand_grain_colour"][1] * proj_readings_sand_grains
-                uv_readings += proj_uv_readings_sand_grains
-
-                proj_red_readings_sand_grains = self.env_variables["sand_grain_colour"][0] * proj_readings_sand_grains
-                red_readings += proj_red_readings_sand_grains
+                red_readings += red_readings_sand_grains * self.env_variables["sand_grain_red_component"]
 
         uv_readings_scaled = self.scale_readings(uv_readings,
                                                  self.env_variables['uv_scaling_factor'] * self.env_variables[

Environment/naturalistic_environment.py
@@ -501,7 +501,7 @@ Sand grain: {self.sand_grain_associated_reward}
         if len(sand_grain_locations) > 0:
             sand_grain_locations_array = np.array(sand_grain_locations) - np.array(self.fish.body.position) + self.board.max_visual_distance
         else:
-            sand_grain_locations_array = np.array([])
+            sand_grain_locations_array = np.empty((0, 2))
 
         self.fish.left_eye.read(full_masked_image, left_eye_pos[0], left_eye_pos[1], self.fish.body.angle, lum_mask,
                                 prey_locations_array, sand_grain_locations_array)