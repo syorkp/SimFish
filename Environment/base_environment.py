f832e460b95e4315349a4f4c36af6fe809491db7
syorkp <sampinky1999@gmail.com>
2023-04-21 13:43:53 +0100

    Assay mode testing.

--- Environment/base_environment.py
+++ Environment/base_environment.py
@@ -534,7 +534,11 @@ class BaseEnvironment:
 
         # Generate impulses
         impulse_types = [0, self.env_variables["slow_impulse_paramecia"], self.env_variables["fast_impulse_paramecia"]]
-        impulses = [impulse_types[gait] for gait in self.paramecia_gaits]
+        try:
+            impulses = [impulse_types[gait] for gait in self.paramecia_gaits]
+        except:
+            print(self.paramecia_gaits)
+            print(impulse_types)
 
         # Do once per step.
         if micro_step == 0:
